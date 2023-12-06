910eea7c0262fb7e83facd8b01a9af963138a620
Toon Spin <toon.spin@gmail.com>
2023-12-07 00:37:41 +0100

    Day 6

---
+++ src/day06.py
@@ -0,0 +1,29 @@
+import math
+import sys
+
+def possibilities(time, distance):
+    spread_squared = time * time - 4 * distance
+    half_spread = .5 * math.sqrt(spread_squared)
+    min_time_holding = math.floor(.5 * time - half_spread)
+    max_time_holding = math.ceil(.5 * time + half_spread)
+    return max_time_holding - min_time_holding - 1
+
+input = sys.stdin.read()
+
+lines = input.splitlines()
+
+times = map(int, lines[0].split()[1:])
+distances = map(int, lines[1].split()[1:])
+
+races = zip(times, distances)
+
+product = 1
+for time, distance in races:
+    product *= possibilities(time, distance)
+
+print(product)
+
+time_part_two = int(''.join(lines[0].split()[1:]))
+distance_part_two = int(''.join(lines[1].split()[1:]))
+
+print(possibilities(time_part_two, distance_part_two))
