2f12d02b4fcf96196d63e06fcd748e67c4946c68
Toon Spin <toon.spin@gmail.com>
2023-12-10 00:32:20 +0100

    Nicer run_all.sh output

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-for day in $(seq -w 24); do
-    if [ -e "src/day${day}.py" ]; then
-        echo "-------------------- DAY $day"
-        python3 "src/day${day}.py" < "data/day${day}.txt"
+divider='----------'
+divider="${divider}${divider}"
+divider="${divider}${divider}"
+divider="${divider}${divider}"
+for day in $(seq 24); do
+    src_file=$(printf "src/day%02d.py" $day)
+    data_file=$(printf "data/day%02d.txt" $day)
+    if [ -e "${src_file}" ]; then
+        caption=" DAY ${day}"
+        echo "${divider:0:-${#caption}}${caption}"
+        python3 "${src_file}" < "${data_file}"
     fi
 done
