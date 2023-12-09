f334172fe835126521cc24e3dd71a90da6a72a5c
Toon Spin <toon.spin@gmail.com>
2023-12-10 00:42:49 +0100

    Slightly more elegant run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-divider='----------'
-divider="${divider}${divider}"
-divider="${divider}${divider}"
-divider="${divider}${divider}"
 for day in $(seq 24); do
     src_file=$(printf "src/day%02d.py" $day)
-    data_file=$(printf "data/day%02d.txt" $day)
     if [ -e "${src_file}" ]; then
-        caption=" DAY ${day}"
-        echo "${divider:0:-${#caption}}${caption}"
-        python3 "${src_file}" < "${data_file}"
+        caption="~DAY~${day}"
+        printf "%80s\n" "${caption}" | tr ' ~' '- '
+        python3 "${src_file}" < "$(printf "data/day%02d.txt" $day)"
     fi
 done
