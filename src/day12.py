bd88319d3c4d01be917030ac753979f53f5b77a9
Toon Spin <toon.spin@gmail.com>
2023-12-15 23:29:57 +0100

    Day 12, part 2

--- src/day12.py
+++ src/day12.py
@@ -1,67 +1,48 @@
-import cProfile
+import functools
 import sys
 
-class SpringRecord:
-    def __init__(self, line):
-        self.record, groups = line.split(' ')
-        self.groups = list(map(int, groups.split(',')))
-    
-    def __repr__(self):
-        groups = ','.join(map(str, self.groups))
-        return f'{self.record} {groups}'
-
-    def part_two(self, factor=5):
-        new_record = '?'.join(factor * [self.record])
-        groups = ','.join(map(str, self.groups))
-        new_groups = ','.join(factor * [groups])
-        return SpringRecord(f'{new_record} {new_groups}')
-
-    def _match(self, gaps):
-        i = 0
-        for gap, group in zip(gaps, self.groups):
-            while gap > 0:
-                c = self.record[i] 
-                if c != '?' and c != '.':
-                    return False
-                gap -= 1
-                i += 1
-            while group > 0:
-                c = self.record[i] 
-                if c != '?' and c != '#':
-                    return False
-                group -= 1
-                i += 1
-        for c in self.record[i:]:
-            if c == '#':
-                return False
-        return True
-
-    def num_possibilities(self, gaps = []):
-        if len(gaps) == len(self.groups):
-            if self._match(gaps):
-                return 1
+@functools.cache
+def possibilities(record, groups):
+    if len(groups) == 0:
+        if any(c == '#' for c in record):
             return 0
-
-        headroom = len(self.record) - sum(gaps) - sum(self.groups)
-        if headroom == 0:
-            return 0
-
-        count = 0
-        if len(gaps) == 0:
-            start = 0
         else:
-            start = 1
+            return 1
+    first_group = groups[0]
+    if len(record) < first_group:
+        return 0
 
-        for l in range(start, headroom + 1):
-            count += self.num_possibilities(gaps + [l])
-
-        return count
+    if record[0] == '?':
+        return possibilities('#' + record[1:], groups) + possibilities('.' + record[1:], groups)
+    if record[0] == '.':
+        return possibilities(record[1:], groups)
     
-input = [SpringRecord(line) for line in sys.stdin.read().splitlines()]
+    if record[:first_group].replace('?', '#') != first_group * '#':
+        return 0
 
-num_possibilities = sum(record.num_possibilities() for record in input)
-print(num_possibilities)
+    if record[first_group:first_group+1] == '#':
+        return 0
 
-input = [record.part_two(3) for record in input]
-cProfile.run('num_possibilities = sum(record.num_possibilities() for record in input)')
-print(num_possibilities)
+    return possibilities(record[first_group+1:], groups[1:])
+
+input = []
+for line in sys.stdin.read().splitlines():
+    record, groups = line.split(' ')
+    groups = tuple(map(int, groups.split(',')))
+    input.append((record, groups))
+
+def part_two(record, groups, count=5):
+    record = [record for _ in range(5)]
+    record = '?'.join(record)
+    return (record, tuple(count * list(groups)))
+
+total = 0
+for record, groups in input:
+    total += possibilities(record, groups)
+print(f'The number of arrangements possible that meet the criteria: {total}')
+
+total = 0
+for record, groups in input:
+    r, g = part_two(record, groups, count=5)
+    total += possibilities(r, g)
+print(f'The number of possibilities after unfolding: {total}')
